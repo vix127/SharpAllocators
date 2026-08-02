b0e2efb6dba66fe326a49e7f9fcdf76a3364d88b
Viktor Stojanovic <viktor2006stojanovic@gmail.com>
2026-08-02 21:18:40 +0200

    Add count parameter to Allocate method and rename size parameter in Realloc method to count

--- src/IAllocator.cs
+++ src/IAllocator.cs
@@ -2,7 +2,7 @@
 
 public unsafe interface IAllocator
 {
-    public T* Allocate<T>() where T : unmanaged;
+    public T* Allocate<T>(long count) where T : unmanaged;
     public void Free<T>(T* pointer) where T : unmanaged;
-    public T* Realloc<T>(T* pointer, long size) where T : unmanaged;
+    public T* Realloc<T>(T* pointer, long count) where T : unmanaged;
 }
