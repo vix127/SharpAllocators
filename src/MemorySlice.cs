ada063f2a766dcc184e8e44d4995645d64945dba
Viktor Stojanovic <viktor2006stojanovic@gmail.com>
2026-08-08 01:04:05 +0200

    Add MemorySlice type

---
+++ src/MemorySlice.cs
@@ -0,0 +1,23 @@
+// Copyright (c) 2026 Viktor Stojanović. All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace SharpAllocators;
+
+public readonly unsafe struct MemorySlice<T> where T : unmanaged
+{
+    public T* Pointer { get; }
+    public nuint Length { get; }
+    public nuint ByteLength => Length * (nuint)sizeof(T);
+
+    public MemorySlice(T* pointer, nuint length)
+    {
+        Pointer = pointer;
+        Length = length;
+    }
+
+    public void Deconstruct(out T* pointer, out nuint lenght)
+    {
+        pointer = Pointer;
+        lenght = Length;
+    }
+}
