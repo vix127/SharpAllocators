668c9c06590879fb1b5181ead640eca73e71d4bc
Viktor Stojanovic <viktor2006stojanovic@gmail.com>
2026-08-07 15:54:39 +0200

    Add AggressiveInlining to all methods for performance.

--- src/NativeAllocator.cs
+++ src/NativeAllocator.cs
@@ -1,19 +1,23 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace SharpAllocators
 {
     public readonly unsafe struct NativeAllocator : IAllocator
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T* Allocate<T>(long count) where T : unmanaged
         {
             return (T*)NativeMemory.Alloc((nuint)(count * sizeof(T)));
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Free<T>(T* pointer) where T : unmanaged
         {
             NativeMemory.Free(pointer);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public T* Reallocate<T>(T* pointer, long count) where T : unmanaged
         {
             return (T*)NativeMemory.Realloc(pointer, (nuint)count);
