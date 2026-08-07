6dfd9fab886fcd4239be611f54a655da2f42648e
Viktor Stojanovic <viktor2006stojanovic@gmail.com>
2026-08-07 22:54:10 +0200

    Convert to file scoped namespace

--- src/NativeAllocator.cs
+++ src/NativeAllocator.cs
@@ -4,26 +4,25 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-namespace SharpAllocators
+namespace SharpAllocators;
+
+public readonly unsafe struct NativeAllocator : IAllocator
 {
-    public readonly unsafe struct NativeAllocator : IAllocator
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T* Allocate<T>(long count) where T : unmanaged
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public T* Allocate<T>(long count) where T : unmanaged
-        {
-            return (T*)NativeMemory.Alloc((nuint)(count * sizeof(T)));
-        }
+        return (T*)NativeMemory.Alloc((nuint)(count * sizeof(T)));
+    }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Free<T>(T* pointer) where T : unmanaged
-        {
-            NativeMemory.Free(pointer);
-        }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Free<T>(T* pointer) where T : unmanaged
+    {
+        NativeMemory.Free(pointer);
+    }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public T* Reallocate<T>(T* pointer, long count) where T : unmanaged
-        {
-            return (T*)NativeMemory.Realloc(pointer, (nuint)count);
-        }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T* Reallocate<T>(T* pointer, long count) where T : unmanaged
+    {
+        return (T*)NativeMemory.Realloc(pointer, (nuint)count);
     }
 }
