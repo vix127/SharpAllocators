2b43c729b726cf7b3480733f29365200549010b8
Viktor Stojanovic <viktor2006stojanovic@gmail.com>
2026-08-12 22:21:23 +0200

    Add copyright and license header to IMemorySlice.cs

--- src/IMemorySlice.cs
+++ src/IMemorySlice.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 Viktor Stojanović. All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
 namespace SharpAllocators;
 
 public unsafe interface IMemorySlice<TPointer, TLength>
